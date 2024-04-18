004c9bae0b0d0620ae690afe3b8ab1521b404bc7
Ignacio Heredia <iheredia@ifca.unican.es>
2024-04-18 14:46:10 +0200

    feat: migrate code from `deephdc`

--- ai4os_demo_app/api.py
+++ ai4os_demo_app/api.py
@@ -21,13 +21,22 @@ To start populating this file, take a look at the docs [1] and at
 an exemplar module [2].
 
 [1]: https://docs.ai4os.eu/
-[2]: https://github.com/ai4os-hub/ai4os-demo_app
+[2]: https://github.com/ai4os-hub/ai4os-demo-app
 """
 
+import base64
+import json
+import math
 from pathlib import Path
 import pkg_resources
+from random import random
+import time
 
-from ai4os_demo_app.misc import _catch_error
+from tensorboardX import SummaryWriter
+from webargs import fields, validate
+
+
+from ai4os_demo_app.misc import _catch_error, launch_tensorboard
 
 
 BASE_DIR = Path(__file__).resolve().parents[1]
@@ -64,22 +73,155 @@ def get_metadata():
     return meta
 
 
-# def warm():
-#     pass
-#
-#
-# def get_predict_args():
-#     return {}
-#
-#
-# @_catch_error
-# def predict(**kwargs):
-#     return None
-#
-#
-# def get_train_args():
-#     return {}
-#
-#
-# def train(**kwargs):
-#     return None
+def get_train_args():
+    arg_dict = {
+        "epoch_num": fields.Int(
+            required=False,
+            missing=10,
+            description="Total number of training epochs",
+        ),
+    }
+    return arg_dict
+
+
+def train(**kwargs):
+    """
+    Dummy training. We just sleep for some number of epochs (1 epoch = 1 second)
+    mimicking some computation taking place.
+    We log some random metrics in Tensorboard to mimic monitoring.
+    """
+    logdir = BASE_DIR / "models" / time.strftime("%Y-%m-%d_%H-%M-%S")
+    writer = SummaryWriter(logdir=logdir, flush_secs=1)
+    launch_tensorboard(logdir=logdir)
+    for epoch in range(kwargs["epoch_num"]):
+        time.sleep(1.)
+        writer.add_scalar(  # fake loss with random noise
+            "scalars/loss",
+            - math.log(epoch + 1) * (1 + random() * 0.2),
+            epoch)
+        writer.add_scalar(  # fake accuracy with random noise (clipped to 1)
+            "scalars/accuracy",
+            min((1 - 1/ (epoch+1)) * (1 + random() * 0.1), 1),
+            epoch)
+    writer.close()
+
+    # Write a fake model file
+    (logdir / 'final_model.hdf5').touch()
+
+    return {"status": "done", "final accuracy": 0.9}
+
+
+def get_predict_args():
+    """
+    TODO: add more dtypes
+    * int with choices
+    * composed: list of strs, list of int
+    """
+    # WARNING: missing!=None has to go with required=False
+    # fmt: off
+    arg_dict = {
+        "demo-str": fields.Str(
+            required=False,
+            missing="some-string",
+        ),
+        "demo-str-choice": fields.Str(
+            required=False,
+            missing="choice2",
+            enum=["choice1", "choice2"],
+        ),
+        "demo-int": fields.Int(
+            required=False,
+            missing=1,
+        ),
+        "demo-int-range": fields.Int(
+            required=False,
+            missing=50,
+            validate=[validate.Range(min=1, max=100)],
+        ),
+        "demo-float": fields.Float(
+            required=False,
+            missing=0.1,
+        ),
+        "demo-bool": fields.Bool(
+            required=False,
+            missing=True,
+        ),
+        "demo-dict": fields.Str(  # dicts have to be processed as strings
+            required=False,
+            missing='{"a": 0, "b": 1}',  # use double quotes inside dict
+        ),
+        "demo-list-of-floats": fields.List(
+            fields.Float(),
+            required=False,
+            missing=[0.1, 0.2, 0.3],
+        ),
+        "demo-image": fields.Field(
+            required=True,
+            type="file",
+            location="form",
+            description="image",  # description needed to be parsed by UI
+        ),
+        "demo-audio": fields.Field(
+            required=True,
+            type="file",
+            location="form",
+            description="audio",  # description needed to be parsed by UI
+        ),
+        "demo-video": fields.Field(
+            required=True,
+            type="file",
+            location="form",
+            description="video",  # description needed to be parsed by UI
+        ),
+    }
+    # fmt: on
+    return arg_dict
+
+
+@_catch_error
+def predict(**kwargs):
+    """
+    Return same inputs as provided. We also add additional fields
+    to test the functionality of the Gradio-based UI [1].
+       [1]: https://github.com/deephdc/deepaas_ui
+    """
+    # Dict are fed as str so have to be converted back
+    kwargs["demo-dict"] = json.loads(kwargs["demo-dict"])
+
+    # Add labels and random probalities to output as mock
+    prob = [random() for _ in range(5)]
+    kwargs["probabilities"] = [i / sum(prob) for i in prob]
+    kwargs["labels"] = ["class2", "class3", "class0", "class1", "class4"]
+
+    # Read media files and return them back in base64
+    for k in ["demo-image", "demo-audio", "demo-video"]:
+        with open(kwargs[k].filename, "rb") as f:
+            media = f.read()
+        media = base64.b64encode(media)  # bytes
+        kwargs[k] = media.decode("utf-8")  # string (in utf-8)
+
+    return kwargs
+
+
+# Schema to validate the `predict()` output
+schema = {
+    "demo-str": fields.Str(),
+    "demo-str-choice": fields.Str(),
+    "demo-int": fields.Int(),
+    "demo-int-range": fields.Int(),
+    "demo-float": fields.Float(),
+    "demo-bool": fields.Bool(),
+    "demo-dict": fields.Dict(),
+    "demo-list-of-floats": fields.List(fields.Float()),
+    "demo-image": fields.Str(
+        description="image"  # description needed to be parsed by UI
+    ),
+    "demo-audio": fields.Str(
+        description="audio"  # description needed to be parsed by UI
+    ),
+    "demo-video": fields.Str(
+        description="video"  # description needed to be parsed by UI
+    ),
+    "labels": fields.List(fields.Str()),
+    "probabilities": fields.List(fields.Float()),
+}
